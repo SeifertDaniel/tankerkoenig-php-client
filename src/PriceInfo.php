17da438458ed55cbce18239c8f4f8d9b01f50347
Daniel Seifert <git@daniel-seifert.com>
2022-04-06 00:11:03 +0200

    price info object can hold null prices if station is closed

--- src/PriceInfo.php
+++ src/PriceInfo.php
@@ -6,27 +6,27 @@ class PriceInfo
 {
 	public string $stationId;
 	public string $status;
-	public float $e5;
-	public float $e10;
-	public float $diesel;
+	public float|null $e5;
+	public float|null $e10;
+	public float|null $diesel;
 
 	public static function fromApiArray(array $array) : self
     {
 		return new static(
 			$array['stationId'],
 			$array['status'],
-			$array[ApiClient::TYPE_E5],
-			$array[ApiClient::TYPE_E10],
-			$array[ApiClient::TYPE_DIESEL]
+			$array[ApiClient::TYPE_E5] ?? null,
+			$array[ApiClient::TYPE_E10] ?? null,
+			$array[ApiClient::TYPE_DIESEL] ?? null
 		);
 	}
 
 	public function __construct(
 		string $stationId,
 		string $status,
-		float $e5,
-		float $e10,
-		float $diesel
+		float|null $e5,
+		float|null $e10,
+		float|null $diesel
 	) {
 		$this->stationId = $stationId;
 		$this->status = $status;
